9ea191181bfac74cf03717d5dc25a17bad62789f
Dickla Ncube <50299861+dicklancube@users.noreply.github.com>
2025-09-14 21:51:29 +0200

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,3 +1,12 @@
-node 2-arguments.js
-node 2-arguments.js Best
-node 2-arguments.js Best School
+#!/usr/bin/node
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 === undefined) {
+  console.log('No argument');
+} else if (arg2 === undefined) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
